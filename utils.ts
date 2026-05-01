599bc86504376a4b99c3a99a9aa06b0c45baea4c
Bereket Dress <bereketfd19@gmail.com>
2026-04-30 23:10:19 -0500

    Delete utils.ts

--- utils.ts
+++
@@ -1,7 +0,0 @@
-export function identity<T>(value: T): T {
-  return value;
-}
-
-export function filterArray<T>(arr: T[], predicate: (item: T) => boolean): T[] {
-  return arr.filter(predicate);
-}
